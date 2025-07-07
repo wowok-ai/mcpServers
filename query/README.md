f453841e226fab7fe7f526267f930655d6459451
wowok <enoloo@163.com>
2025-07-07 09:53:59 +0800

    fix

--- query/README.md
+++ query/README.md
@@ -1,5 +1,11 @@
 # WoWok Query MCP Server
-A server for handling queries in the WOWOK protocol.
+A server for handling queries in the WOWOK protocol:
+1. The account, personal information, address names and tags, etc. that are recorded on the local device. 
+2. Basic information of the WoWok protocol. 
+3. Data and tables of the objects on the chain. 
+4. Chain events of the WoWok objects. 
+5. Permission information of any address for a certain Permission object. 
+6. Passive receiving Payment objects of the Order or Treasury objects.
 
 ## What is WoWok？
 Create, collaborate, and transact on your own terms with the AI-driven web3 collaboration protocol.
--- query/src/index.ts
+++ query/src/index.ts
@@ -13,7 +13,13 @@ A.WOWOK.Protocol.Instance().use_network(A.WOWOK.ENTRYPOINT.testnet);
 const server = new Server({
     name: "wowok_query_mcp_server",
     version: "1.1.14",
-    description: `A server for handling queries in the WOWOK protocol.`,
+    description: `A server for handling queries in the WOWOK protocol.
+    1. The account, personal information, address names and tags, etc. that are recorded on the local device. 
+    2. Basic information of the WoWok protocol. 
+    3. Data and tables of the objects on the chain. 
+    4. Chain events of the WoWok objects. 
+    5. Permission information of any address for a certain Permission object. 
+    6. Passive receiving Payment objects of the Order or Treasury objects.`,
   },{
     capabilities: {
       prompts: { },
