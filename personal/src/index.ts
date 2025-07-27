f5a380dc9f985f71b3e04da4a56af3ca6be61b93
wowok <enoloo@163.com>
2025-07-27 18:16:32 +0800

    fix

--- personal/src/index.ts
+++ personal/src/index.ts
@@ -16,7 +16,7 @@ A.WOWOK.Protocol.Instance().use_network(A.WOWOK.ENTRYPOINT.testnet);
 // Create server instance
 const server = new Server({
     name: "wowok_personal_mcp_server",
-    version: "1.2.41",
+    version: "1.2.42",
     description: `${A.CallPersonalSchemaDescription} - A server for handling Personal calls in the WOWOK protocol. ${A.NoticeFieldsOrder}`,
   },{
     capabilities: {
