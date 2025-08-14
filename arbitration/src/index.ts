7f9054d31cd5769af7f494c19cc888b943ae733a
wowok <enoloo@163.com>
2025-08-14 18:43:19 +0800

    fix

--- arbitration/src/index.ts
+++ arbitration/src/index.ts
@@ -15,7 +15,7 @@ A.WOWOK.Protocol.Instance().use_network(A.WOWOK.ENTRYPOINT.testnet);
 // Create server instance
 const server = new Server({
     name: "wowok_arbitration_mcp_server",
-    version: "1.2.48",
+    version: "1.2.49",
     description: `${A.CallArbitrationSchemaDescription} - A server for handling Arbitration calls in the WOWOK protocol. ${A.NoticeFieldsOrder}`,
   },{
     capabilities: {
--- service/src/index.ts
+++ service/src/index.ts
@@ -15,7 +15,7 @@ A.WOWOK.Protocol.Instance().use_network(A.WOWOK.ENTRYPOINT.testnet);
 // Create server instance
 const server = new Server({
     name: "wowok_service_mcp_server",
-    version: "1.2.48",
+    version: "1.2.49",
     description: `${A.CallServiceSchemaDescription} - A server for handling Service calls in the WOWOK protocol. ${A.NoticeFieldsOrder}`,
   },{
     capabilities: {
