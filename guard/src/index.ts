02db6a6c7ed20994b0c393ec45af670aad8128e9
wowok <enoloo@163.com>
2025-08-21 16:42:20 +0800

    fix

--- guard/src/index.ts
+++ guard/src/index.ts
@@ -15,7 +15,7 @@ A.WOWOK.Protocol.Instance().use_network(A.WOWOK.ENTRYPOINT.testnet);
 // Create server instance
 const server = new Server({
     name: "wowok_guard_mcp_server",
-    version: "1.2.50",
+    version: "1.2.51",
     description: `${A.CallGuardSchemaDescription} - A server for handling Guard calls in the WOWOK protocol. ${A.NoticeFieldsOrder}`,
   },{
     capabilities: {
